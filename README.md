600075d31985a5b54531010e9829220594c8b13f
hota1024 <hotalog@hota1024.com>
2019-12-22 14:06:14 +0900

    :tada: Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+# 🚀 sar
+
+Start local server.
+
+## Installation
+
+```bash
+$ yarn global add sar
+# or
+$ npm global install sar
+```
+
+## Usage
+
+Simple usage.
+
+```bash
+$ sar [dir] # Start local server in dir
+```
+
+SPA mode.
+
+```bash
+$ sar [dir] --spa
+```
+
+
+## Help
+
+```bash
+Usage:
+  $ start [dir]
+
+Commands:
+  [dir]  Start static local server.
+
+For more info, run any command with the `--help` flag:
+  $ start --help
+
+Options:
+  -p, --port     port
+  -s, --spa      spa flag
+  -h, --help     Display this message
+  -v, --version  Display version number
+```
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+// import rewrite from 'express-urlrewrite'
+import { cac } from 'cac'
+import chalk from 'chalk'
+import * as path from 'path'
+const { version } = require('../package.json')
+
+const cli = cac('sar')
+
+cli
+  .command('[dir]', 'Start static local server.')
+  .option('-p, --port', 'port')
+  .option('-s, --spa', 'spa flag')
+  .action((dir = '.', options: any) => {
+    console.log(chalk`{cyan sar-cli ${version}}`)
+    const app = express()
+    const host = 'localhost'
+    const port = options.port || 80
+    const url = `http://${host}:${port}`
+    const spaEntry = 'index.html'
+
+    app.use(express.static(dir))
+    if (options.spa) {
+      app.get('*', (_request, response) => {
+        response.sendFile(path.resolve(dir, spaEntry))
+      })
+    }
+
+    app.listen(port, () => {
+      console.log(chalk`🚀  Server is currently online on ${url}`)
+
+      if (options.spa) {
+        console.log(
+          chalk`{cyan ✅  SPA mode enabled. All paths write to ${spaEntry} }`
+        )
+      }
+    })
+  })
+
+cli.help()
+cli.version(version)
+cli.parse()
